265098a58c9ef7b971da051a79f8c557593c5069
Luke Youngdong Ji <zephyrosjyd@gmail.com>
2020-11-30 18:11:48 +0900

    ✨ chapter 6

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -6,8 +6,8 @@
       <div class="user-profile__follower-count">
         <strong>Followers: </strong> {{ followers }}
       </div>
-      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
-        <label for="newTwoot"><strong>New Twoot</strong></label>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot" :class="{ '--exceeded': newTwootCharacterCount > 180 }">
+        <label for="newTwoot"><strong>New Twoot</strong> ({{ newTwootCharacterCount }}/180)</label>
         <textarea id="newTwoot" rows="4" v-model="newTwootContent" />
 
         <div class="user-profile__create-twoot-type">
@@ -70,8 +70,8 @@ export default {
     }
   },
   computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
+    newTwootCharacterCount() {
+      return this.newTwootContent.length;
     }
   },
   methods: {
@@ -96,47 +96,57 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .user-profile {
   display: grid;
   grid-template-columns: 1fr 3fr;
-  /* width: 100%; */
-  gap: 20px;
+  grid-gap: 20px;
   padding: 50px 5%;
-}
 
-.user-profile__user-panel {
-  display: flex;
-  flex-direction: column;
-  margin-right: 50px;
-  padding: 20px;
-  background-color: white;
-  border-radius: 5px;
-  border: 1px solid #dfe3e8;
-}
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
 
-.user-profile__admin-badge {
-  background: purple;
-  color: white;
-  border-radius: 5px;
-  margin-right: auto;
-  padding: 0 10px;
-  margin-bottom: 20px;
-}
+    h1 {
+      margin: 0;
+    }
 
-h1 {
-  margin: 0;
-}
+    .user-profile__admin-badge {
+      background: purple;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      padding: 0 10px;
+      margin-bottom: 20px;
+    }
 
-.user-profile__twoots-wrapper {
-  display: grid;
-  grid-gap: 10px
-}
+    .user-profile__create-twoot {
+      border-top: 1px solid #dfe3e8;
+      padding-top: 20px;
+      display: flex;
+      flex-direction: column;
 
-.user-profile__create-twoot {
-  border-top: 1px solid #dfe3e8;
-  padding-top: 20px;
-  display: flex;
-  flex-direction: column;
+      &.--exceeded {
+        color: red;
+        border-color: red;
+
+        button {
+          border: none;
+          background-color: red;
+          color: white;
+        }
+      }
+    }
+  }
+
+  .user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px
+  }
 }
 </style>
